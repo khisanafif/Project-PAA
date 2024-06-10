4e2e229e407787b46fa3b1700efde549876ece78
khisanafif <khisanafif32@gmail.com>
2024-06-10 07:28:17 +0700

    Co-authored-by: khisanafif <khisanafif32@gmail.com>

--- lib/service/remote_service/remote_banner_service.dart
+++ lib/service/remote_service/remote_banner_service.dart
@@ -2,13 +2,22 @@ import 'package:http/http.dart' as http;
 import 'package:my_grocery/const.dart';
 
 class RemoteBannerService {
-  var client = http.Client();
-  var remoteUrl = '$baseUrl/api/banners';
-  
-  Future<dynamic> get() async {
-    var response = await client.get(
-      Uri.parse('$remoteUrl?populate=image')
-    );
-    return response;
+  final http.Client _client = http.Client();
+  final String _remoteUrl = '$baseUrl/api/banners';
+
+  Future<http.Response> get() async {
+    try {
+      final http.Response response = await _client.get(
+        Uri.parse('$_remoteUrl?populate=image'),
+      );
+      return response;
+    } catch (e) {
+      print('Error occurred: $e');
+      rethrow; // rethrow the exception for higher-level error handling
+    }
+  }
+
+  void close() {
+    _client.close(); // close the client when it's no longer needed
   }
 }
--- lib/service/remote_service/remote_category_service.dart
+++ lib/service/remote_service/remote_category_service.dart
@@ -2,14 +2,11 @@ import 'package:http/http.dart' as http;
 import 'package:my_grocery/const.dart';
 
 class RemoteCategoryService {
-  var client = http.Client();
-  var remoteUrl = '$baseUrl/api/categories';
-  
-  Future<dynamic> get() async {
-    var response = await client.get(
-      Uri.parse('$remoteUrl?populate=image')
-    );
+  final http.Client client = http.Client();
 
+  Future<dynamic> get() async {
+    final remoteUrl = '$baseUrl/api/categories';
+    final response = await client.get(Uri.parse('$remoteUrl?populate=image'));
     return response;
   }
 }
